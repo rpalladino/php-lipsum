5911821114be0e0ae9bd98520eb80b4a756a01af
Rocco Palladino <rocky@uchicago.edu>
2015-05-09 10:52:17 -0500

    Add console command

---
+++ src/Command.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Rpalladino\Lipsum;
+
+use Symfony\Component\Console\Command\Command as ConsoleCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Command extends ConsoleCommand
+{
+    protected function configure()
+    {
+        $this->setName("lipsum")
+             ->setDescription("Generate lipsum text")
+             ->addOption(
+                 'what',
+                 'w',
+                 InputOption::VALUE_REQUIRED,
+                 'The kind of text to generate: paras, words, bytes, lists',
+                 'paras'
+             )
+             ->addOption(
+                 'amount',
+                 'a',
+                 InputOption::VALUE_REQUIRED,
+                 'The amount of text to generate',
+                 5
+             )
+             ->addOption(
+                 'start-with-lipsum',
+                 's',
+                 InputOption::VALUE_NONE,
+                 'Start generated text with "Lorem ipsum dolor sit amet."',
+                 null
+             );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $lipsum = new Lipsum;
+
+        $what   = $input->getOption("what");
+        $amount = $input->getOption("amount");
+        $start  = $input->getOption("start-with-lipsum");
+        
+        $output->writeln($lipsum->getText(compact('what', 'amount', 'start')));
+    }
+}
---
+++ tests/CommandTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Rpalladino\Lipsum\Test;
+
+use Rpalladino\Lipsum\Command;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @group integration
+ */
+class CommandTest extends \PHPUnit_Framework_TestCase
+{
+    private $command;
+    private $commandTester;
+
+    public function setUp()
+    {
+        $app = new Application;
+        $app->add(new Command);
+
+        $this->command = $app->find("lipsum");
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    /**
+     * @test
+     */
+    public function itExecutes()
+    {
+        $this->commandTester->execute(['command' => $this->command->getName()]);
+        $this->assertInternalType('string', $this->commandTester->getDisplay());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanStartWithLoremIpsum()
+    {
+        $this->commandTester->execute([
+            "command" => $this->command->getName(),
+            "--start-with-lipsum" => true
+        ]);
+        $this->assertStringStartsWith('Lorem ipsum', $this->commandTester->getDisplay());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanStartWithoutLoremIpsum()
+    {
+        $this->commandTester->execute([
+            "command" => $this->command->getName(),
+        ]);
+        $this->assertStringStartsNotWith('Lorem ipsum', $this->commandTester->getDisplay());
+    }
+
+    /**
+     * @test
+     */
+    public function itGetsSpecifiedAmountOfParagraphs()
+    {
+        $this->commandTester->execute([
+            "command" => $this->command->getName(),
+            "--amount" => 3,
+            "--what" => "paras"
+        ]);
+        $this->assertCount(3, explode("\n", trim($this->commandTester->getDisplay())));
+    }
+
+    /**
+     * @test
+     */
+    public function itGetsSpecifiedAmountOfWords()
+    {
+        $this->commandTester->execute([
+            "command" => $this->command->getName(),
+            "--amount" => 5,
+            "--what" => "words"
+        ]);
+        $this->assertCount(5, explode(" ", trim($this->commandTester->getDisplay())));
+    }
+
+    /**
+     * @test
+     */
+    public function itGetsSpecifiedAmountOfBytes()
+    {
+        $this->commandTester->execute([
+            "command" => $this->command->getName(),
+            "--amount" => 256,
+            "--what" => "bytes"
+        ]);
+        $this->assertEquals(256, strlen(trim($this->commandTester->getDisplay())));
+    }
+
+    /**
+     * @test
+     */
+    public function itGetsSpecifiedAmountOfLitItems()
+    {
+        $this->commandTester->execute([
+            "command" => $this->command->getName(),
+            "--amount" => 3,
+            "--what" => "lists"
+        ]);
+        $this->assertCount(3, explode("\n", trim($this->commandTester->getDisplay())));
+    }
+}
